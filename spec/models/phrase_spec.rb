a6255e3bfa6f0b446b4898644314d71fdc1d1163
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 16:48:20 +0900

    2-5 各モデルのバリデーションをテスト

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Phrase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    context '正常な場合' do
+      it 'バリデーションを通過' do
+        note = Note.new(title: 'テスト')
+        phrase = Phrase.new(note: note, expression_en: 'Test', expression_ja: 'テスト')
+        expect(phrase).to be_valid
+      end
+    end
+  end
 end
