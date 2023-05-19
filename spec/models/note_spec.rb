b4ff1c6aeabfd2d785dded2155902273027cc3c7
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 18:03:57 +0900

    2-8 各モデルのテストファイルを修正

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   describe 'バリデーション' do
     context '正常な場合' do
+      let(:note_filled) { FactoryBot.create(:note) }
+
       it 'バリデーションを通過' do
-        note = Note.new(title: 'テスト')
-        expect(note).to be_valid
+        expect(note_filled).to be_valid
       end
     end
   end
