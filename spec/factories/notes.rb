5aa6105d01c945f358c8e056f4116230d852b5c5
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 17:46:08 +0900

    2-7 Note,Phraseのファクトリを定義

---
+++ spec/factories/notes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :note do
+    title { 'メモ１' }
+    text_en { 'There are apples on that tree.' } 
+    text_ja { 'あの木にはりんごがなっています。' }
+    free_text { 'テスト用' }
+  end
+end
---
+++ spec/factories/phrases.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :phrase do
+    expression_en { 'Apple' } 
+    expression_ja { 'りんご' }
+    note
+  end
+end
