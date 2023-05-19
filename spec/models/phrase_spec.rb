196af609836456ab0e54fe9e63a71921faff8f08
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 18:03:57 +0900

    2-8 各モデルのテストファイルを修正

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Phrase, type: :model do
   describe 'バリデーション' do
     context '正常な場合' do
+      let(:phrase_filled) { FactoryBot.create(:phrase) }
+
       it 'バリデーションを通過' do
-        note = Note.new(title: 'テスト')
-        phrase = Phrase.new(note: note, expression_en: 'Test', expression_ja: 'テスト')
-        expect(phrase).to be_valid
+        expect(phrase_filled).to be_valid
       end
     end
   end
