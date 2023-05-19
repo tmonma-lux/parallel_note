d6538135e4ef2f451763303765e351cd95a69ce3
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 18:47:21 +0900

    2-8 インスタンス生成をbuildに変更，異常系を追加

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Phrase, type: :model do
   describe 'バリデーション' do
-    context '正常な場合' do
-      let(:phrase_filled) { FactoryBot.create(:phrase) }
+    let(:phrase_filled) { FactoryBot.build(:phrase) }
+    let(:phrase_blank_en) { FactoryBot.build(:phrase, expression_en: nil) }
 
+    context '正常な場合' do
       it 'バリデーションを通過' do
         expect(phrase_filled).to be_valid
+      end
+    end
+
+    context '英語部分が未入力の場合' do
+      it 'バリデーションエラー' do
+        expect(phrase_blank_en).to be_invalid
       end
     end
   end
