f788f90acbb31006b5e74fb63ddf5ec0a296f4c2
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-19 18:51:00 +0900

    2-9 Phraseテストの異常系項目を追加

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Phrase, type: :model do
   describe 'バリデーション' do
     let(:phrase_filled) { FactoryBot.build(:phrase) }
     let(:phrase_blank_en) { FactoryBot.build(:phrase, expression_en: nil) }
+    let(:phrase_blank_ja) { FactoryBot.build(:phrase, expression_ja: nil) }
 
     context '正常な場合' do
       it 'バリデーションを通過' do
@@ -14,6 +15,12 @@ RSpec.describe Phrase, type: :model do
     context '英語部分が未入力の場合' do
       it 'バリデーションエラー' do
         expect(phrase_blank_en).to be_invalid
+      end
+    end
+
+    context '日本語部分が未入力の場合' do
+      it 'バリデーションエラー' do
+        expect(phrase_blank_ja).to be_invalid
       end
     end
   end
