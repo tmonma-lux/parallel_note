95b1cf9b96a4078bfee68b535907a1d964ea35fd
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-22 09:33:37 +0900

    2-11 テストファイルにおけるlet処理の位置を修正

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -2,23 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Phrase, type: :model do
   describe 'バリデーション' do
-    let(:phrase_filled) { FactoryBot.build(:phrase) }
-    let(:phrase_blank_en) { FactoryBot.build(:phrase, expression_en: nil) }
-    let(:phrase_blank_ja) { FactoryBot.build(:phrase, expression_ja: nil) }
-
     context '正常な場合' do
+      let(:phrase_filled) { FactoryBot.build(:phrase) }
+
       it 'バリデーションを通過' do
         expect(phrase_filled).to be_valid
       end
     end
 
     context '英語部分が未入力の場合' do
+      let(:phrase_blank_en) { FactoryBot.build(:phrase, expression_en: nil) }
+
       it 'バリデーションエラー' do
         expect(phrase_blank_en).to be_invalid
       end
     end
 
     context '日本語部分が未入力の場合' do
+      let(:phrase_blank_ja) { FactoryBot.build(:phrase, expression_ja: nil) }
+
       it 'バリデーションエラー' do
         expect(phrase_blank_ja).to be_invalid
       end
