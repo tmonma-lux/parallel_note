09cbfc4c307aa324740fe7ea9b86ad8ff5b5754d
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-24 14:16:30 +0900

    7-3 notesコントローラに新規作成用のメソッドを追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,4 +4,25 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def new
+    @note_form = NoteForm.new
+  end
+
+  def create
+    @note_form = NoteForm.new(note_form_params)
+
+    if @note_form.save
+      redirect_to root_url, notice: 'メモの保存が完了しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_form_params
+    params.require(:note_form).permit(:title, :text_en, :text_ja, :free_text,
+                                      :expression_en, :expression_ja)
+  end
 end
