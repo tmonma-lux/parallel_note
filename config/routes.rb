da758144d8263417ee2fe2735afb685fdd38a70a
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-30 11:57:16 +0900

    8-1 ルーティングにeditとupdateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :notes, only: [:index, :show, :new, :create]
+  resources :notes, only: [:index, :show, :new, :create, :edit, :update]
   root to: 'notes#index'
   
   get 'phrases', to: 'phrases#index'
