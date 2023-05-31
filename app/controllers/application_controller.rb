fef55db04ae7b599228e6e9aa0553ea4a8519a55
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-31 14:57:55 +0900

    40-1 404/500エラー発生時のリダイレクト用メソッドを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :redirect_on_404
+  rescue_from ActionController::RoutingError, with: :redirect_on_404
+  rescue_from Exception, with: :redirect_on_500
+
+  def redirect_on_404
+    redirect_to notes_url, alert: 'ページが見つかりませんでした。'
+  end
+
+  def redirect_on_500
+    redirect_to notes_url, alert: 'サーバーでエラーが発生しました。'
+  end
 end
