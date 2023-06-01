6d6f132206c9e440cbaeb0d1edd0073cfddad35f
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-01 16:04:47 +0900

    10-11 RuboCopに従いコードを修正

--- app/forms/note_form.rb
+++ app/forms/note_form.rb
@@ -36,7 +36,7 @@ class NoteForm
       # すでに登録されている語句を削除
       @note.phrases.map(&:destroy!)
       # 語句とその意味がともに空欄のデータを削除
-      phrases.delete_if { |phrase_attrs| phrase_attrs[:expression_en].blank? && phrase_attrs[:expression_ja].blank? }
+      delete_blank_phrases
       # 語句が存在している場合は登録
       insert_new_phrases
     end
@@ -48,6 +48,10 @@ class NoteForm
 
   private
 
+  def delete_blank_phrases
+    phrases.delete_if { |phrase_attrs| phrase_attrs[:expression_en].blank? && phrase_attrs[:expression_ja].blank? }
+  end
+
   def insert_new_phrases
     return if phrases.empty?
 
@@ -58,12 +62,15 @@ class NoteForm
   def validate_phrases
     phrases.each do |phrase_attrs|
       if phrase_attrs[:expression_en].present? && phrase_attrs[:expression_ja].blank?
-        errors.add(:base, "#{NoteForm.human_attribute_name(:expression_en)}に対応する" \
-                          "#{NoteForm.human_attribute_name(:expression_ja)}を入力してください")
+        add_phrase_blank_err(:expression_en, :expression_ja)
       elsif phrase_attrs[:expression_en].blank? && phrase_attrs[:expression_ja].present?
-        errors.add(:base, "#{NoteForm.human_attribute_name(:expression_ja)}に対応する" \
-                          "#{NoteForm.human_attribute_name(:expression_en)}を入力してください")
+        add_phrase_blank_err(:expression_ja, :expression_en)
       end
     end
+  end
+
+  def add_phrase_blank_err(attr1, attr2)
+    errors.add(:base, "#{NoteForm.human_attribute_name(attr1)}に対応する" \
+                      "#{NoteForm.human_attribute_name(attr2)}を入力してください")
   end
 end
