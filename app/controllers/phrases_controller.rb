368e058281158d30a40a0b87ea00c87e3b901391
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-05 11:11:19 +0900

    13-5 メモの検索結果，語句の一覧・検索結果にページネーションを追加

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -2,12 +2,12 @@
 
 class PhrasesController < ApplicationController
   def index
-    @phrases = Phrase.preload(:note)
+    @phrases = Phrase.preload(:note).page(params[:page])
   end
 
   def search
     redirect_to phrases_url if params[:query].empty?
-    @phrases = Phrase.search(params[:query]).preload(:note)
+    @phrases = Phrase.search(params[:query]).preload(:note).page(params[:page])
     @query = params[:query]
   end
 end
