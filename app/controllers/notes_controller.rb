d1a9dcfc20a5161f4d14bf9882c45d8c82ec5fea
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-06 09:50:35 +0900

    54-1 Note検索メソッドにタグのpreloadを追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -11,7 +11,7 @@ class NotesController < ApplicationController
 
   def search
     check_search_form(params[:query], params[:tags])
-    @notes = Note.search(params[:query], params[:tags]).page(params[:page])
+    @notes = Note.search(params[:query], params[:tags]).preload(:tags).page(params[:page])
     @query = params[:query]
   end
 
