a79661e12fe264eff442e4940c9f81c1b2abdb0e
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-07 14:21:25 +0900

    14-13 言語切替え用の変数名を変更

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,6 +3,6 @@
 class QuizzesController < ApplicationController
   def show
     @phrases = Phrase.order('RAND()').limit(4).preload(:note)
-    @q_lang = params[:q_lang] || 'en'
+    @question_language = params[:question_language] || 'en'
   end
 end
