a22fb37b03479ba56b9eaedfa368106a2e1e57f0
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-08 10:03:43 +0900

    58-5 解答表示用メソッドの名前を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-function changeText(event) {
+function revealAnswer(event) {
   event.target.classList.remove('mask-answer');
   event.target.textContent = this.text;
 }
@@ -10,6 +10,6 @@ function changeText(event) {
 document.addEventListener('turbo:load', function () {
   const answerboxes = document.getElementsByClassName('mask-answer');
   for(let i = 0; i < answerboxes.length; i++) {
-    answerboxes[i].addEventListener('click', {handleEvent: changeText, text: answerboxes[i].dataset.answer});
+    answerboxes[i].addEventListener('click', {handleEvent: revealAnswer, text: answerboxes[i].dataset.answer});
   }
 });
