39f86fbaca3e27b01955f3568f8aa6b40db222cb
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-15 11:06:59 +0900

    20-2 フォーム追加用関数を実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,3 +13,32 @@ document.addEventListener('turbo:load', function () {
     answerboxes[i].addEventListener('click', {handleEvent: revealAnswer, text: answerboxes[i].dataset.answer});
   }
 });
+
+
+function addPhraseForm() {
+  const phraseForms = document.querySelector('.phrase-forms');
+  const form = createPhraseForm();
+  phraseForms.appendChild(form);
+}
+
+function createPhraseForm() {
+  var formWrapper = document.createElement('div');
+  formWrapper.classList.add('row', 'mb-3');
+
+  formWrapper.innerHTML = `
+    <div class="col">
+      <input class="form-control" type="text" name="note_form[phrases[][expression_en]]" id="note_form_phrases[][expression_en]">
+    </div>
+    <div class="col">
+      <input class="form-control" type="text" name="note_form[phrases[][expression_ja]]" id="note_form_phrases[][expression_ja]">
+    </div>
+  `;
+
+  return formWrapper;
+}
+
+document.addEventListener('turbo:load', function () {
+  const button = document.getElementById('add-phrase-button');
+  if (!button) { return false; }
+  button.addEventListener('click', {handleEvent: addPhraseForm});
+});
