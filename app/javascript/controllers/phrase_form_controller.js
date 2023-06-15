6e64178ae103db0ee740179e21ac62bf25f58eca
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-15 16:27:35 +0900

    20-8 Stimulus内のController名を変更

--- app/javascript/controllers/phrase_form_controller.js
+++ app/javascript/controllers/phrase_form_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="phraseform"
+// Connects to data-controller="phrase-form"
 export default class extends Controller {
   static targets = ["form_wrapper"];
 
