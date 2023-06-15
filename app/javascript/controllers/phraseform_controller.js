1d4c8bedca35f7afbcf68bd618f5e4594858c399
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-15 15:38:54 +0900

    20-6 Stimulusを使用した形式に変更

---
+++ app/javascript/controllers/phraseform_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="phraseform"
+export default class extends Controller {
+  static targets = ["form_wrapper"];
+
+  addRow() {
+    const row = this._createRow();
+    this.form_wrapperTarget.appendChild(row);
+  }
+
+  _createRow() {
+    var row = document.createElement('div');
+    row.classList.add('row', 'mb-3');
+  
+    row.innerHTML = `
+      <div class="col">
+        <input class="form-control" type="text" name="note_form[phrases[][expression_en]]" id="note_form_phrases[][expression_en]">
+      </div>
+      <div class="col">
+        <input class="form-control" type="text" name="note_form[phrases[][expression_ja]]" id="note_form_phrases[][expression_ja]">
+      </div>
+    `;
+  
+    return row;
+  }
+}
