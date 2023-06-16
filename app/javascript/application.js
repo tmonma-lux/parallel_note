8c664d3def3487ea808f33951f12b1c338e33935
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-16 16:51:57 +0900

    21-9 StimulusのControllerとして記述

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import "./src/jquery"
-import "jquery-ui-dist";
-import "./tag-it"
-
-$(document).on("turbo:load", function(){
-  $(".tag_form").tagit();
-})
---
+++ app/javascript/controllers/tag_form_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+import "../src/jquery"
+import "jquery-ui-dist";
+import "../tag-it"
+
+// Connects to data-controller="tag-form"
+export default class extends Controller {
+  connect() {
+    const tagForm = this.element.querySelector("#tag_field");
+    $(tagForm).tagit();
+  }
+}
