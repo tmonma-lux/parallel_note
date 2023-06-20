b71f16a8fde7c15bf3e954841c5c23c42b723f22
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-06-20 14:54:22 +0900

    22-4 ajax通信によるインクリメンタルサーチ機能を実装

--- app/javascript/controllers/tag_form_controller.js
+++ app/javascript/controllers/tag_form_controller.js
@@ -6,8 +6,29 @@ import "../tag-it"
 // Connects to data-controller="tag-form"
 export default class extends Controller {
   connect() {
-    const tagForm = this.element.querySelector("#tag_field");
+    const tagForm = this.element.querySelector('#tag_field');
     $(tagForm).tagit();
-    $("ul.tagit").addClass('form-control w-auto');
+    $('ul.tagit').addClass('form-control w-auto');
+
+    $(document).on('keyup', '.tagit', function() {
+      const tag_input = $('.ui-widget-content.ui-autocomplete-input').val();
+  
+      $.ajax({
+        type: 'GET',
+        url: 'fetch_tag_suggestions',
+        data: { tag_input: tag_input },
+        dataType: 'json'
+      })
+      .done(function(data){
+        if(tag_input.length) {
+          const tag_list = data.map(function(val){
+            return val.name
+          });
+          $(tagForm).tagit({
+            availableTags: tag_list
+          });
+        }
+      });
+    });
   }
 }
